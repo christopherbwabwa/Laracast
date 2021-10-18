410ab8d9dc86bb570067e328f83f41a67fb5b2d6
Christopher Bwabwa <christopherbwabwa@gmail.com>
2021-10-19 00:25:09 +0300

    Starting with comments

---
+++ app/Http/Controllers/SessionsController.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Validation\ValidationException;
+
+class SessionsController extends Controller
+
+{
+    public function create()
+    {
+        return view('sessions.create');
+    }
+
+    public function store()
+    {
+        $attributes = request()->validate([
+            'email' => 'required|email',
+            'password' => 'required'
+        ]);
+
+        if(! Auth::attempt($attributes))
+        {
+            throw ValidationException::withMessages(
+                ['email' => 'Your provided crendetials could not be verified!']
+            );
+            
+        }
+
+        session()->regenerate();
+        return redirect('/')->with('success', 'Welcome back!');
+    }
+
+    public function destroy()
+    {
+        Auth::logout();
+
+        return redirect('/')->with('success', 'Goodbye!');
+    }
+}
---
+++ resources/views/components/post-comment.blade.php
@@ -0,0 +1,17 @@
+<article class="flex bg-gray-100 border border-gray-200 p-6 rounded-xl space-x-4">
+    <div class="flex-shrink-0">
+        <img src="https://i.pravatar.cc/60" alt="" width="60" height="60" class="rounded-xl">
+    </div>
+    <div>
+        <header class="mb-4">
+            <h3 class="font-bold">John Doe</h3>
+            <p class="text-xs">Posted <time> 8 months ago </time> </p>
+        </header>
+
+        <p>
+            Filosowe, denkers en skrywers bepleit die teorie van verteenwoordigende parlemente op 'n tyd 
+            Wanneer mense onbewustelik is opgepas soos skape deur konings, sultans en oorwinnaars
+            Selfs as klein in getal, gemeenskappe of groepe te vorm een nasie, ongeag van die individuele
+        </p>
+    </div>
+</article>
