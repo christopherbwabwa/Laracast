83cdb0bedd05ae1e6dd6b19914969ca5884ae857
Christopher Bwabwa <christopherbwabwa@gmail.com>
2024-05-13 21:59:22 +0200

    status column added to posts

--- resources/views/admin/posts/index.blade.php
+++ resources/views/admin/posts/index.blade.php
@@ -22,6 +22,10 @@
                                             </div>
                                         </td>
 
+                                        <td class="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
+                                            <span
+                                                class="bg-red-300 p-1 rounded-sm text-xs text-white hover:text-green-600">{{ $post->status }}</span>
+                                        </td>
 
                                         <td class="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
                                             <a href="/admin/posts/{{ $post->id }}/edit"
